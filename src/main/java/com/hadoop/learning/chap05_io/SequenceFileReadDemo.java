4ae9a00f2fb1e84f3dce26df93250eda258b74e5
fanyuexiang <fanyuexiang@jd,com>
2020-03-10 08:44:43 +0800

    Hadoop权威指南第五章 读取sequenceFile

--- src/main/java/com/hadoop/learning/chap05_io/SequenceFileReadDemo.java
+++ src/main/java/com/hadoop/learning/chap05_io/SequenceFileReadDemo.java
@@ -3,6 +3,7 @@ package com.hadoop.learning.chap05_io;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.util.ReflectionUtils;
@@ -30,8 +31,16 @@ public class SequenceFileReadDemo {
                     ReflectionUtils.newInstance(reader.getKeyClass(), configuration);
             Writable value = (Writable)
                     ReflectionUtils.newInstance(reader.getValueClass(), configuration);
+            long position = reader.getPosition();
+            while (reader.next(key, value)) {
+                String syncSeen = reader.syncSeen() ? "*" : "";
+                System.out.printf("[%s%s]\t%s\t%s\n", position, syncSeen, key, value);
+                position = reader.getPosition(); // beginning of next record
+            }
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            IOUtils.closeStream(reader);
         }
     }
 
